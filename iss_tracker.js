f3acd0306b4832b3d0ff452ff22df5ca2869332d
Parth Sharma <137429675+parthCJ@users.noreply.github.com>
2024-09-28 21:46:45 +0530

    Update iss_tracker.js

--- iss_tracker.js
+++ iss_tracker.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://0.0.0.0:${PORT}`);
-});
+});// can remove this part
 function initializeISSTracker() {
     let map, issMarker;
 
