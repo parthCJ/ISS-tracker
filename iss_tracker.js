d641c685a55f03d03a0cb25e97b1319d677dc00a
Parth Sharma <137429675+parthCJ@users.noreply.github.com>
2024-09-28 21:46:26 +0530

    initial commit

--- iss_tracker.js
+++ iss_tracker.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
 function initializeISSTracker() {
     let map, issMarker;
 
