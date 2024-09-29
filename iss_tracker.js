f03dee6020b041be7db5142ff499b0764ce5b0e0
Parth Sharma <137429675+parthCJ@users.noreply.github.com>
2024-09-29 10:15:01 +0530

    Update iss_tracker.js

--- iss_tracker.js
+++ iss_tracker.js
@@ -1,17 +1,3 @@
-const express = require('express');
-const app = express();
-
-// Get the port from Railway environment variables
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 function initializeISSTracker() {
     let map, issMarker;
 
